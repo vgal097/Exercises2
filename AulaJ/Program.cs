92263b541b9003d400c8bde4f68f4980dd1e9f93
Vitor Garcia <105651529+vgal097@users.noreply.github.com>
2022-06-06 10:56:17 -0300

    Add files via upload

---
+++ AulaJ/Program.cs
@@ -0,0 +1,17 @@
+﻿int x = 1;
+
+while (x == 1)
+{
+    Console.WriteLine("Digite sua idade: ");
+    int idade = int.Parse(Console.ReadLine());
+
+    if (idade >= 18)
+    {
+        Console.WriteLine("Está apto a dirigir");
+        x++;
+    }
+    else
+    {
+        Console.WriteLine("Não possui 18 anos");
+    }
+}
---
+++ Exercicio 2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Digite sua nota");
+double nota = double.Parse(Console.ReadLine());
+
+if (nota >= 7)
+{
+    Console.WriteLine("aprovado");
+}  
+else if (nota >= 5 && nota < 7)
+{
+    Console.WriteLine("Recuperação");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
+
+/*string aprovacao = nota >= 7 ? "aprovado" : "reprovado";
+
+Console.WriteLine(aprovacao);*/
